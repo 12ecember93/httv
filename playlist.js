356500507692577a578fbbf82fda16e87bedc824
12ecember93 <272778388+12ecember93@users.noreply.github.com>
2026-06-09 12:43:48 +0700

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,11 +1,13 @@
 const playlistData =
 {
   "VTV": [
-    { "name": "VTV1 HD", "short": "VTV1", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv1-hd_165657381026.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/fnxch2/vtv1hd_abr.smil/chunklist.m3u8" },
-    { "name": "VTV2 HD", "short": "VTV2", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv2-hd_16565576783.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/fnxch2/vtv2hd_abr.smil/chunklist.m3u8" },
-    { "name": "VTV3 HD", "short": "VTV3", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv3-hd_165657381668.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/fnxch2/vtv3hd_abr.smil/chunklist.m3u8" },
+    { "name": "VTV1 HD", "short": "VTV1", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv1-hd_165657381026.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/live/media/vtv1/live247-hls-avc/vtv1-avc1_5600000=10000-mp4a_131600=20000.m3u8" },
+    { "name": "VTV2 HD", "short": "VTV2", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv2-hd_16565576783.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/live/media/vtv2/live247-hls-avc/vtv2-avc1_5600000=10000-mp4a_131600=20000.m3u8" },
+    { "name": "VTV3 HD", "short": "VTV3", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv3-hd_165657381668.png?w=200&c=0&fmt=webp", "url": "https://live-a.fptplay53.net/live/media/vtv3/live247-hls-avc/vtv3-avc1_5600000=10000-mp4a_131600=20000.m3u8" },
     { "name": "VTV4 HD", "short": "VTV4", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv4-hd_165657382285.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/fnxch2/vtv4hd_abr.smil/chunklist.m3u8" },
     { "name": "VTV5 HD", "short": "VTV5", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv5-hd_165657382858.png?w=200&c=0&fmt=webp", "url": "https://live-a.fptplay53.net/live/media/VTV5HD/live_hls_avc/index.m3u8" },
+    { "name": "VTV5TNB HD", "short": "VTV5 TNB", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/d96ee31b-eeef-4f4a-8245-0a9c0006bc46.jpg?width=500", "url": "https://live.fptplay53.net/fnxhd1/vtv5tnb_vhls.smil/chunklist_b5000000.m3u8
+" },
     { "name": "VTV6 HD", "short": "VTV6", "logo": "https://vtvgo-assets.vtvdigital.vn/assets/images/v2/channel/20260607/2026060709/cWXXGTRTIZ-VTV6-THUMBNAIL-KENHVTVgo-500x281.webp", "url": "https://live-a.fptplay53.net/live/media/vtv6/live247-hls-avc/vtv6-avc1_5600000=10000-mp4a_131600=20000.m3u8" },
     { "name": "VTV7 HD", "short": "VTV7", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv7-hd_165655773646.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/fnxhd1/vtv7hd_vhls.smil/chunklist_b5000000.m3u8" },
     { "name": "VTV8 HD", "short": "VTV8", "logo": "https://images.fptplay53.net/media/channels/icon_channel_vtv8-hd_165657496167.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/vtv8hd_vhls.smil/chunklist.m3u8" },
@@ -14,8 +16,8 @@ const playlistData =
   ],
   "HTV & THVL": [
     { "name": "Vĩnh Long 1", "short": "THVL1", "logo": "https://images.fptplay53.net/media/channels/icon_channel_thvl1_165657384745.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd2/vinhlong1_vhls.smil/chunklist_b5000000.m3u8" },
-    { "name": "HTV7 HD", "short": "HTV7", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv7-hd_16565582880.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzch2/htv7hd_abr.smil/chunklist.m3u8" },
-    { "name": "HTV9 HD", "short": "HTV9", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv9-hd_165655829886.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/htv9hd_vhls.smil/chunklist.m3u8" },
+    { "name": "HTV7 HD", "short": "HTV7", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv7-hd_16565582880.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/htv7hd_vhls.smil/chunklist_b5000000.m3u8" },
+    { "name": "HTV9 HD", "short": "HTV9", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv9-hd_165655829886.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/htv9hd_vhls.smil/chunklist_b5000000.m3u8" },
     { "name": "HTV1 HD", "short": "HTV1", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv1_165655706945.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/htv1_hls.smil/chunklist.m3u8" },
     { "name": "HTV2 HD", "short": "HTV2", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv2_165657612334.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/htv2hd_vhls.smil/chunklist.m3u8" },
     { "name": "HTV3 HD", "short": "HTV3", "logo": "https://images.fptplay53.net/media/channels/icon_channel_htv3_165655827991.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd1/htv3_hls.smil/chunklist.m3u8" },
