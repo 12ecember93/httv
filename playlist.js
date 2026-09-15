e14b71c5c6e6ce1b9784ef36e6ade9f68529c84b
12ecember93 <272778388+12ecember93@users.noreply.github.com>
2026-09-15 17:10:56 +0700

    Add files via upload

--- playlist.js
+++ playlist.js
@@ -4,7 +4,6 @@ const playlistData =
     { "name": "VTV1", "short": "VTV1", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/5de59236-89a2-4387-8664-f2fc27608093.jpg?width=500", "url": "https://live.fptplay53.net/live/media/vtv1/live247-hls-avc/index.m3u8" },
     { "name": "VTV3", "short": "VTV3", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/292a3f4e-c6ee-4858-9c49-f61dde69a854.jpg?width=500", "url": "https://live.fptplay53.net/live/media/vtv3/live247-hls-avc/index.m3u8" },
     { "name": "VTV6", "short": "VTV6", "logo": "https://vtvgo-assets.vtvdigital.vn/assets/images/v2/channel/20260607/2026060709/cWXXGTRTIZ-VTV6-THUMBNAIL-KENHVTVgo-500x281.webp", "url": "https://live-a.fptplay53.net/live/media/vtv6/live247-hls-avc/index.m3u8" },
-    { "name": "ONSPORT1", "short": "ONSPORT1", "logo": "https://imgvlive.vtvcab.vn/720X405/fd8acdaa-f6c8-4101-bb3b-660a83d9eaf4.jpg", "url": "https://liveh12vng.vtvprime.vn/hls/ONSPORT1/04.m3u8" },
     { "name": "VTV2", "short": "VTV2", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/5ac4a9fb-6da7-4785-aab5-ae85e471d869.jpg?width=500", "url": "https://live-a.fptplay53.net/live/media/vtv2/live247-hls-avc/index.m3u8" },
     { "name": "VTV4", "short": "VTV4", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/f161041d-3a3f-4c0a-88f2-ae97f8052ee7.jpg?width=500", "url": "https://live-a.fptplay53.net/live/media/vtv4/live247-hls-avc/index.m3u8" },
     { "name": "VTV5", "short": "VTV5", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/d84c3036-3cc4-4c45-911f-8d1c5025cd93.jpg?width=500", "url": "https://live-a.fptplay53.net/live/media/vtv5/live247-hls-avc/index.m3u8" },
@@ -14,17 +13,17 @@ const playlistData =
     { "name": "VTV9", "short": "VTV9", "logo": "https://vtvgo-images.vtvdigital.vn/images/20230626/7f951ab0-a128-44a9-84cd-ba8faf108ad3.jpg?width=500", "url": "https://live-a.fptplay53.net/live/media/vtv9/live247-hls-avc/index.m3u8" },
     { "name": "VTV10", "short": "VTV10", "logo": "https://vtvgo-assets.vtvdigital.vn/assets/images/v2/channel/20260330/2026033009/1lmelwJ48A-VTV10-THUMBNAIL-KENH-VTVgo-480x270.webp", "url": "https://live-a.fptplay53.net/live/media/vtv10/live247-hls-avc/index.m3u8" },
     { "name": "Vietnam Today", "short": "Vietnam Today", "logo": "https://vtvgo-assets.vtvdigital.vn/assets/images/v2/channel/20250905/2025090515/S1CWNGr2gI-VIETNAMTODAY-THUMBNAIL-KENHVTVgo-500x281.webp", "url": "https://live.fptplay53.net/fnxhd1/vntoday_vhls.smil/chunklist_b5000000.m3u8" },
-    { "name": "Bong Da Lau", "short": "Bong Da Lau","url": "https://live05.meung.app/live/16226575.m3u8" }
     
   ],
   "Others": [
     { "name": "THVL1", "short": "THVL1", "logo": "https://images.fptplay53.net/media/channels/icon_channel_thvl1_165657384745.png?w=200&c=0&fmt=webp", "url": "https://live.fptplay53.net/epzhd2/vinhlong1_vhls.smil/chunklist_b5000000.m3u8" },
-    { "name": "HTV7", "short": "HTV7", "logo": "https://img.hplus.com.vn/355x200/poster/2018/07/16/928755-665121-HTV7HD.png", "url": "https://live.fptplay53.net/epzhd1/htv7hd_vhls.smil/chunklist_b5000000.m3u8" },
-    { "name": "HTV9", "short": "HTV9", "logo": "https://img.hplus.com.vn/355x200/poster/2018/07/16/422430-665121-HTV9HD.png", "url": "https://live.fptplay53.net/epzhd1/htv9hd_vhls.smil/chunklist_b5000000.m3u8" },
-    { "name": "HTV1", "short": "HTV1", "logo": "https://img.hplus.com.vn/355x200/poster/2018/07/16/185783-505482-HTV1.png", "url": "https://live.fptplay53.net/epzhd1/htv1_hls.smil/chunklist.m3u8" },
-    { "name": "HTV2", "short": "HTV2", "logo": "https://img.hplus.com.vn/355x200/poster/2018/06/05/904719-HTV2HD.png", "url": "https://live.fptplay53.net/epzhd1/htv2hd_vhls.smil/chunklist.m3u8" },
-    { "name": "HTV3", "short": "HTV3", "logo": "https://img.hplus.com.vn/355x200/poster/2018/06/05/750643-HTV3.png", "url": "https://live.fptplay53.net/epzhd1/htv3_hls.smil/chunklist.m3u8" },
-    { "name": "MUTV", "short": "MUTV", "logo": "https://upload.wikimedia.org/wikipedia/en/b/b8/MUTV_logo.png", "url": "http://line.ciao-ott.net:80/play/live.php?mac=00:1A:79:C8:7B:30&stream=1312824&extension=ts&play_token=3ouilCRXf9" },
-    { "name": "Canal+ Foot", "short": "Canal+ Foot", "logo": "https://upload.wikimedia.org/wikipedia/commons/e/eb/Canal%2BFoot.png", "url": "http://line.watchtivo-8k.com:80/play/live.php?mac=00:1A:79:3F:0C:96&stream=941405&extension=ts&play_token=ODWyLygMWo" }   
+    { "name": "HTV7", "short": "HTV7", "logo": "https://img.hplus.com.vn/355x200/poster/2018/07/16/928755-665121-HTV7HD.png", "url": "https://live.fptplay53.net/live/media/htv7/live247-hls-avc/htv7-avc1_5600000=10000-mp4a_131600=20000.m3u8" },
+    { "name": "HTV9", "short": "HTV9", "logo": "https://img.hplus.com.vn/355x200/poster/2018/07/16/422430-665121-HTV9HD.png", "url": "https://live-a.fptplay53.net/live/media/htv9/live247-hls-avc/htv9-avc1_5600000=10000-mp4a_131600=20000.m3u8" },
+    { "name": "S? Ki?n FPTPLAY"", "short": "FPT1", "logo": "https://images.fptplay53.net/media/photo/OTT/2025/05/26/logowebsite4x_1748221195181.png", "url": "https://vips-livecdn.fptplay.net/live/media/event-01/hls_avc_v6/index.m3u8" },
+    { "name": "S? Ki?n FPTPLAY"", "short": "FPT2", "logo": "https://images.fptplay53.net/media/photo/OTT/2025/05/26/logowebsite4x_1748221195181.png", "url": "https://vips-livecdn.fptplay.net/live/media/event-02-4k/dash_hvc/index.mpd" },
+    { "name": "S? Ki?n FPTPLAY"", "short": "FPT3", "logo": "https://images.fptplay53.net/media/photo/OTT/2025/05/26/logowebsite4x_1748221195181.png", "url": "https://vips-livecdn.fptplay.net/live/media/event-02-4k/hls_avc_v6/index.m3u8?sign=1789274527-MDc1OTczMzQ-0-13f38d14c7fac4175df565d3c223972a" },
+    { "name": "S? Ki?n FPTPLAY"", "short": "FPT4", "logo": "https://images.fptplay53.net/media/photo/OTT/2025/05/26/logowebsite4x_1748221195181.png", "url": "https://vips-livecdn.fptplay.net/live/media/event-02/hls_avc_v6/index.m3u8" },
+    { "name": "MUTV", "short": "MUTV", "logo": "https://m.media-amazon.com/images/I/611NwkDJjOL.png", "url": "http://line.ciao-ott.net:80/play/live.php?mac=00:1A:79:C8:7B:30&stream=1312824&extension=ts&play_token=3ouilCRXf9" },
+    { "name": "Canal+ Foot", "short": "Canal+ Foot", "logo": "https://upload.wikimedia.org/wikipedia/commons/e/eb/Canal%2BFoot.png", "url": "http://line.watchtivo-8k.com:80/play/live.php?mac=00:1A:79:3F:0C:96&stream=941405&extension=ts&play_token=ODWyLygMWo" }
   ]
 }
